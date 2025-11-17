aa55d5bd21139604bffbcb319798e730f047e318
csabee1979 <kallai.csaba@gmail.com>
2025-11-17 17:48:42 +0100

    Update package dependencies and configuration for Streamlit application
    - Removes unnecessary comments from `packages.txt` to streamline the file.
    - Introduces `requirements-alt.txt` for alternative dependency management.
    - Adjusts `requirements.txt` to specify a version range for `psycopg2-binary`.
    - Adds `runtime.txt` to define the Python version for the application.
    - Modifies `.streamlit/config.toml` to disable CORS and XSRF protection, and sets logger level to "info".
    - Cleans up `database.py` for improved readability in SSL mode configuration.

--- database/database.py
+++ database/database.py
@@ -33,9 +33,7 @@ class DatabaseConfig:
         self.database = os.getenv("DB_NAME")
         self.user = os.getenv("DB_USER")
         self.password = os.getenv("DB_PASSWORD")
-        self.sslmode = os.getenv(
-            "DB_SSLMODE", "require"
-        )  # Default to 'require' if not set
+        self.sslmode = os.getenv("DB_SSLMODE", "require")
 
         # Validate that all required parameters are loaded
         missing_params = []
